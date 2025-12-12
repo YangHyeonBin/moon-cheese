de5f625e7191e8d8ef505e1027848ee2be94894e
Yang HyeonBin <idgusqls0506@naver.com>
2025-12-12 17:29:56 +0900

    add: 상품 상세, 추천 상품 조회 쿼리

--- src/remotes/queries/product.ts
+++ src/remotes/queries/product.ts
@@ -1,22 +1,44 @@
 import { queryOptions } from '@tanstack/react-query';
-import { getProductList, getRecentProductList } from '../product';
+import { getProductDetail, getProductList, getRecentProductList } from '../product';
 
-export const productQueries = {
-  // 쿼리 키
+/**
+ * Product 쿼리 키
+ */
+export const productQueryKeys = {
   all: () => ['product'],
-  productListKey: () => [...productQueries.all(), 'list'],
-  recentProductListKey: () => [...productQueries.all(), 'recent'],
+  productList: () => [...productQueryKeys.all(), 'list'],
+  detail: (productId: string) => [...productQueryKeys.all(), 'detail', productId],
 
-  // 쿼리 옵션
+  recentProductList: () => [...productQueryKeys.all(), 'recent'],
+
+  recommendProductList: (productId: string) => [...productQueryKeys.all(), 'recommend', productId],
+};
+
+/**
+ * Product 쿼리 옵션
+ */
+export const productQueryOptions = {
+  productList: () =>
+    queryOptions({
+      queryKey: productQueryKeys.productList(),
+      queryFn: getProductList,
+    }),
+
+  productDetail: (productId: string) =>
+    queryOptions({
+      queryKey: productQueryKeys.detail(productId),
+      queryFn: () => getProductDetail(productId),
+    }),
+
   recentProductList: () =>
     queryOptions({
-      queryKey: productQueries.recentProductListKey(),
+      queryKey: productQueryKeys.recentProductList(),
       queryFn: getRecentProductList,
     }),
 
-  productList: () =>
+  recommendProductList: (productId: string) =>
     queryOptions({
-      queryKey: productQueries.productListKey(),
-      queryFn: getProductList,
+      queryKey: productQueryKeys.recommendProductList(productId),
+      queryFn: () => {},
     }),
 };
